6f2fdb7642f5220ad2a99ae6a92d1db52e1455d1
Prudhvi1654 <160996904+Prudhvi1654@users.noreply.github.com>
2025-08-23 20:29:55 +0530

    All Turtle basics .py
    Added turtle basics all-in-one code

---
+++ All Turtle basics .py
@@ -0,0 +1,66 @@
+import turtle
+# Setup Screen
+
+screen = turtle.Screen()
+screen.bgcolor("lightpink")
+screen.title("Turtle Basics - All in One")
+
+# Create a turtle
+pen = turtle.Turtle()
+pen.shape("turtle")
+pen.color("blue")
+pen.speed(2)
+
+
+# Movement Basics
+pen.forward(100)
+pen.left(90)
+pen.forward(100)
+pen.right(90)
+pen.backward(100)
+pen.right(90)
+pen.forward(190)
+
+# Pause before next section
+pen.penup()
+pen.goto(-150, 0)
+pen.pendown()
+
+# Square
+pen.color("red")
+for _ in range(4):
+    pen.forward(100)
+    pen.left(90)
+
+# Pause before next section
+pen.penup()
+pen.goto(190, 0)
+pen.pendown()
+
+# Circle
+pen.color("green")
+pen.circle(60)
+
+# Pause before next section
+pen.penup()
+pen.goto(0, -150)
+pen.pendown()
+
+# Star
+pen.color("purple")
+for _ in range(5):
+    pen.forward(100)
+    pen.right(144)
+
+# Pause before next section
+pen.penup()
+pen.goto(-50, 150)
+pen.pendown()
+
+# Writing Text
+pen.color("black")
+pen.write("Thanks for watching my Turtle !!", font=("Times new roman", 5, "italic"))
+
+# Hide Turtle & Keep Window Open
+pen.hideturtle()
+screen.mainloop()
